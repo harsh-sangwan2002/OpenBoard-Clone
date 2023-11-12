81099969004d2fc6f11f6fbf353e840363a1cbf9
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-11-12 08:20:28 +0530

    added

--- canvas.js
+++ canvas.js
@@ -2,29 +2,77 @@ let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let pencilColorCont = document.querySelectorAll('.pencil-color');
+let pencilWidthEle = document.querySelector('.pencil-width');
+let eraserWidthEle = document.querySelector('.eraser-width');
+
+let penColor = "red";
+let eraserColor = "white";
+let penWidth = pencilWidthEle.value;
+let eraserWidth = eraserWidthEle.value;
+
 let mouseDown = false;
 let tool = canvas.getContext("2d");
 
-tool.strokeStyle = "red";
-tool.lineWidth = "3";
+tool.strokeStyle = penColor;
+tool.lineWidth = penWidth;
 
 canvas.addEventListener("mousedown", e => {
 
     mouseDown = true;
-    tool.beginPath();
-    tool.moveTo(e.clientX, e.clientY);
+    beginPath({
+        x:e.clientX,
+        y:e.clientY
+    })
 })
 
 canvas.addEventListener("mousemove", e => {
 
     if (mouseDown) {
 
-        tool.lineTo(e.clientX, e.clientY);
-        tool.stroke();
+        drawStroke({
+            x:e.clientX,
+            y:e.clientY
+        })
     }
 })
 
 canvas.addEventListener("mouseup", e => {
 
     mouseDown = false;
+})
+
+function beginPath(strokeObj) {
+
+    tool.beginPath();
+    tool.moveTo(strokeObj.x, strokeObj.y);
+}
+
+function drawStroke(strokeObj) {
+
+    tool.lineTo(strokeObj.x, strokeObj.y);
+    tool.stroke();
+
+}
+
+pencilColorCont.forEach(colorEle=>{
+
+    colorEle.addEventListener("click",e=>{
+
+        let color = colorEle.classList[0];
+        penColor = color;
+        tool.strokeStyle = penColor;
+    })
+})
+
+pencilWidthEle.addEventListener("change",e=>{
+
+    penWidth = pencilWidthEle.value;
+    tool.lineWidth = penWidth;
+})
+
+eraserWidthEle.addEventListener("change",e=>{
+
+    eraserWidth = eraserWidthEle.value;
+    tool.lineWidth = eraserWidth;
 })
